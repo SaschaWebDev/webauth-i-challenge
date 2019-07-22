1e6f58d29f57612fcbbf8f84024df3f37ee5d630
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-22 23:22:21 +0200

    imported the session module and configured it

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
 const moment = require('moment');
+const session = require('express-session');
 
 const UsersRouter = require('./api/users/user-router.js');
 
 const server = express();
+
+const sessionConfig = {
+  name: 'monkey', // default is 'sid' by changing the name we don't expose the  used library
+  secret: 'keep it secret, keep it safe!', // should be in environment variable
+  cookie: {
+    maxAge: 1000 * 30, // cookie will stay valid for 30 seconds
+    secure: false, // cookie only over HTTPS? Always true in production!
+    httpOnly: true, // No JavaScript on the client get access to the cookie
+  },
+  resave: false, // Recreate a session even if it hasn't changed?
+  saveUninitialized: false, // GDPR compliance does not allow setting cookies by default! Only true when user accepted!
+};
 
 server.get('/', (req, res) => {
   res.send(
@@ -15,6 +28,7 @@ server.get('/', (req, res) => {
 server.use(Requestlogger);
 server.use(express.json());
 server.use(helmet());
+server.use(session(sessionConfig));
 server.use('/api/users', UsersRouter);
 
 function Requestlogger(req, res, next) {
